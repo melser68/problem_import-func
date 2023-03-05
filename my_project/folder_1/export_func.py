c04f77f3e689572e21a8b09be996241b6002f6c5
SERHII MELNYK <melser68@i.ua>
2023-03-05 08:29:27 +0100

    problem modeling

---
+++ my_project/folder_1/export_func.py
@@ -0,0 +1,3 @@
+
+def print_tryple(text):
+    print(text * 3)
---
+++ my_project/folder_2/main_modul.py
@@ -0,0 +1,4 @@
+from folder_1.export_func import print_tryple as pt #тут помилки вроді як нема
+
+pt('test')  # ModuleNotFoundError: No module named 'folder_1'
+
